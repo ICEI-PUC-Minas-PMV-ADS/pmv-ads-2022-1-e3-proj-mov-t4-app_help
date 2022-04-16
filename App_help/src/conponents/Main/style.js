6bbb1799a542339fb558e764dfcd08c5a9eef6e1
Gerald Santos <79993851+Gerald-0@users.noreply.github.com>
2022-04-16 19:03:18 -0300

    Add files via upload

---
+++ App_help/src/conponents/Main/style.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    boxTitle:{
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    textTitle:{
+        color:"#FFFFFF",
+        fontSize:36,
+        textAlign:"center",
+        
+    },
+    Button:{
+        backgroundColor:"#3DD39F",
+        width:350,
+        height:45,
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:25,
+        marginBottom:20,
+    },
+    text:{
+        color:"#FFFFFF",
+        fontSize:18,
+    },
+})
+
+export default styles
